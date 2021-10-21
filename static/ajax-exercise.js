0aaf3f33814b696e7881afeb27211d11562046b3
Kim <knghiem03@gmail.com>
2021-10-21 11:11:55 -0500

    transform all regular functions to inline

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -2,31 +2,32 @@
 
 // PART 1: SHOW A FORTUNE
 
-function showFortune(evt) {
+$('#get-fortune-button').on('click', () => {
   // TODO: get the fortune and show it in the #fortune-text div
   $.get('/fortune', resp => {
     $('#fortune-text').html(resp);
     //only .html works for thise because of the html formatting that gets returned by the functin.
   });
   
-}
+});
 
-$('#get-fortune-button').on('click', showFortune);
+// $('#get-fortune-button').on('click', showFortune);
 
 // PART 2: SHOW WEATHER
 
-function showWeather(evt) {
+$('#weather-form').on('submit',  evt => {
   evt.preventDefault();
 
   const url = '/weather.json';
   const formData = {zipcode: $('#zipcode-field').val()};
 
-  $.get('/weather.json', formData, resp => {
+  $.get(url, formData, resp => {
     //either way works bt if using the [] you need to use the "" around forecast.
     //alert(resp.forecast);
-    alert(resp["forecast"]);
+    //alert(resp["forecast"]);
+    $('#weather-info').html(resp.forecast);
   });
-}
+});
 
 $('#weather-form').on('submit', showWeather);
 
